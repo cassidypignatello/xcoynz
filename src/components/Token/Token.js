cd1e31988e385ee1019cf9c4480d5369a0eeed4f
Cassidy <cassidy.pignatello@gmail.com>
2019-04-30 22:26:36 +0800

    revises Token component

--- src/components/Token/Token.js
+++ src/components/Token/Token.js
@@ -9,12 +9,18 @@ export default class Token extends Component {
         <Row className='justify-content-center'>
           <Col
             xs='auto'
-            lg={{ span: 6, offset: 1 }}
+            lg={{ span: 6 }}
             className={`${styles.tokenPurpose}`}>
+            <p className={styles.tokenTitle}>Token Purpose & Appeal</p>
+            <p className={styles.tokenDescription}>
+              The purpose of the XCZ token is to promote features that elevate functionality of the day-to-day XCOYNZ business, in turn driving the business forward and hence increasing the value of the token by ensuring that it has a fundamental use.<br /><br />
+              Individuals will reap the benefits of having XCZ tokens ready to be used for exchange fee discounts and a faster, streamlined withdrawal mechanism. Over the next 18 months, there will be a drive to future-proof the market share that XCOYNZ captures, ensuring optimum level of token recycling safeguards business expansion and ultimately enhances mass appeal for the token.<br /><br />
+              To this effect, XCOYNZ will implement a programme to burn a set proportion of collected tokens, writing off a fixed share of our business profits with limits and frequency to be decided once all components of the platform have been launched and the business if fully functional.
+            </p>
           </Col>
           <Col
             xs='auto'
-            lg={{ span: 5, offset: 0 }}
+            lg={{ span: 6 }}
             className={`${styles.tokenProperties}`}>
           </Col>
         </Row>
