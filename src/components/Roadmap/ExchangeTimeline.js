9808c2ce49e5e25199baea1d5723b0188e359282
Cassidy <cassidy.pignatello@gmail.com>
2019-05-01 17:06:54 +0800

    sets up Roadmap navigation

---
+++ src/components/Roadmap/ExchangeTimeline.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { Row, Col } from 'react-bootstrap'
+import styles from './ExchangeTimeline.module.css'
+
+export default class ExchangeTimeline extends Component {
+  render() {
+    return (
+      <Row>
+        <Col></Col>
+      </Row>
+    )
+  }
+}
--- src/components/Roadmap/Roadmap.js
+++ src/components/Roadmap/Roadmap.js
@@ -1,17 +1,66 @@
 import React, { Component } from 'react'
-import { Container, Row } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 import styles from './Roadmap.module.css'
+import ExchangeTimeline from './ExchangeTimeline.js'
 
 export default class Roadmap extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: 'Exchange'
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(e) {
+    this.setState({
+      active: e.target.dataset.name
+    })
+  }
+
   render() {
     return (
       <Container
-        className={styles.roadmap} 
+        className={styles.roadmap}
         id='roadmap'>
         <Row className='justify-content-center'>
-          <p className={styles.title}>Our Roadmap</p>
+          <Col>
+            <p className={styles.title}>Our Roadmap</p>
+          </Col>
         </Row>
-        <Row></Row>
+        <ExchangeTimeline />
+        <Row className={`${styles.navigation} justify-content-center`}>
+          <Col lg='auto'>
+            <div className={this.state.active === 'Exchange' ? `${styles.active}` : null}>
+              <p
+                data-name='Exchange'
+                className={`${styles.navlink}`}
+                onClick={this.handleClick}>
+                Exchange Roadmap
+              </p>
+            </div>
+          </Col>
+          <Col lg='auto'>
+            <div className={this.state.active === 'SAW' ? `${styles.active}` : null}>
+              <p
+                data-name='SAW'
+                className={styles.navlink}
+                onClick={this.handleClick}>
+                SAW Roadmap
+              </p>
+            </div>
+          </Col>
+          <Col lg='auto'>
+            <div className={this.state.active === 'Payment Gateway' ? `${styles.active}` : null}>
+              <p
+                data-name='Payment Gateway'
+                className={styles.navlink}
+                onClick={this.handleClick}>
+                Payment Gateway Roadmap
+              </p>
+            </div>
+          </Col>
+        </Row>
       </Container>
     )
   }
