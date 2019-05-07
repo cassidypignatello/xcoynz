f652ac1b0798af0250f8d5ce354f71478d7ab43f
Cassidy <cassidy.pignatello@gmail.com>
2019-05-07 20:20:14 +0800

    completes Blog section

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -5,6 +5,7 @@ import Slider from 'react-slick'
 import 'slick-carousel/slick/slick-theme.css';
 import 'slick-carousel/slick/slick.css';
 import BlogPostCard from './BlogPostCard.js'
+import PrevArrow from './PrevArrow.js'
 import NextArrow from './NextArrow.js'
 import images from '../../assets/images/images.js'
 
@@ -15,7 +16,22 @@ export default class Blog extends Component {
       speed: 500,
       slidesToShow: 3,
       slidesToScroll: 1,
+      prevArrow: <PrevArrow />,
       nextArrow: <NextArrow />,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2,
+          }
+        },
+        {
+          breakpoint: 540,
+          settings: {
+            slidesToShow: 1,
+          }
+        },
+      ]
     }
     return (
       <Container
--- src/components/Blog/PrevArrow.js
+++ src/components/Blog/PrevArrow.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import styles from './Arrow.module.css'
 
-const NextArrow = (props) => {
+const PrevArrow = (props) => {
   const { onClick } = props
   return (
     <div
-      className={`${styles.arrow} ${styles.nextArrow}`}
+      className={`${styles.arrow} ${styles.prevArrow}`}
       onClick={onClick}>
     </div>
   )
 }
 
-export default NextArrow
+export default PrevArrow
