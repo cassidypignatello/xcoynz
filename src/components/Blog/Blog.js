8f918ccfd1b7c76b44c94e2c49b77f3e457617c3
Cassidy <cassidy.pignatello@gmail.com>
2019-05-07 13:40:11 +0800

    installs react-slick and dependencies

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 import styles from './Blog.module.css'
+import Slider from 'react-slick'
 
 export default class Blog extends Component {
   render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 3,
+      slidesToScroll: 3
+    }
     return (
       <Container
         id='blog'
@@ -14,7 +22,25 @@ export default class Blog extends Component {
           </Col>
         </Row>
         <Row>
-          <Col></Col>
+          <Col>
+            <Slider {...settings}>
+              <div>
+                <h3>1</h3>
+              </div>
+              <div>
+                <h3>2</h3>
+              </div>
+              <div>
+                <h3>3</h3>
+              </div>
+              <div>
+                <h3>4</h3>
+              </div>
+              <div>
+                <h3>5</h3>
+              </div>
+            </Slider>
+          </Col>
         </Row>
       </Container>
     )
