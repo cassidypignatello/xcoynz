6a14c30deccedb4295d62633aa1c990f34948589
Cassidy <cassidy.pignatello@gmail.com>
2019-05-07 16:18:20 +0800

    refactors into BlogPostCard component

---
+++ src/components/Blog/BlogPostCard.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import { Card, Button } from 'react-bootstrap'
+import styles from './Blog.module.css'
+
+
+export default class BlogPostCard extends Component {
+  render() {
+    const { image, date, title, description } = this.props
+    return (
+      <Card className={styles.card}>
+        <Card.Img variant="top" src={image} />
+        <Card.Body className={styles.body}>
+          <Card.Text className={`${styles.date} d-flex align-items-center`}>
+            <span className={styles.calendar}></span>
+            {date}
+          </Card.Text>
+          <Card.Title className={styles.postTitle}>{title}</Card.Title>
+          <Card.Text className={styles.description}>{description}</Card.Text>
+          <Button
+            variant='light'
+            className={styles.button}>
+            Read More
+            <span className={`${styles.arrow} mr-5`}></span>
+          </Button>
+        </Card.Body>
+      </Card>
+    )
+  }
+}
