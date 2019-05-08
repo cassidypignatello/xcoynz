91f58ba7447b7326363448c1a562db9fd7c183c0
Cassidy <cassidy.pignatello@gmail.com>
2019-05-08 21:45:05 +0800

    adds product FAQ section and completes FAQs component

--- src/components/FAQs/FAQs.js
+++ src/components/FAQs/FAQs.js
@@ -169,11 +169,11 @@ export default class FAQs extends Component {
                     </Col>
                     <Col
                       xs={1}
-                      className={isOpen === '1' ? `${styles.close}` : `${styles.open}`}
+                      className={isOpen === '5' ? `${styles.close}` : `${styles.open}`}
                       onClick={this.openQuestion}
-                      data-index='1'>
+                      data-index='5'>
                     </Col>
-                    { isOpen === '1' ?
+                    { isOpen === '5' ?
                       <Row>
                         <Col lg={10}>
                           <p className={styles.answer}>The Pre-Sale is in place to ensure we have a great start and meet the highest of standards for things such as preliminary marketing and full compliance for all legal matters. In short, for a successful ICO.</p>
@@ -372,6 +372,148 @@ export default class FAQs extends Component {
                   </Row>
                 </Col>
               </Row>
+            </Col> : null
+          }
+          { active === 'Product' ?
+            <Col lg={7} className={styles.questions}>
+              <Row>
+                <Col>
+                  <Row className={`${styles.questionBox} mb-4`}>
+                    <Col xs={10}>
+                      <p className={styles.question}>Why is XCOYNZ being developed using blockchain technology?</p>
+                    </Col>
+                    <Col
+                      xs={1}
+                      className={isOpen === '15' ? `${styles.close}` : `${styles.open}`}
+                      onClick={this.openQuestion}
+                      data-index='15'>
+                    </Col>
+                    { isOpen === '15' ?
+                      <Row>
+                        <Col lg={10}>
+                          <p className={styles.answer}>The XCOYNZ Platform is being developed using blockchain in order to avail and unlock discounts and bonuses when using the XCZ token on the XCOYNZ platform.</p>
+                        </Col>
+                      </Row> :
+                      null
+                    }
+                  </Row>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <Row className={`${styles.questionBox} mb-4`}>
+                    <Col xs={10}>
+                      <p className={styles.question}>Why is the XCOYNZ Exchange better than other exchanges?</p>
+                    </Col>
+                    <Col
+                      xs={1}
+                      className={isOpen === '16' ? `${styles.close}` : `${styles.open}`}
+                      onClick={this.openQuestion}
+                      data-index='16'>
+                    </Col>
+                    { isOpen === '16' ?
+                      <Row>
+                        <Col lg={10}>
+                          <p className={styles.answer}>It is more than an exchange, it is a Platform with the benefit of innovative and unique features such as the XCOYNZ SAW, Payment Gateway and Smart Card. The XCOYNZ Exchange allows an Individual to exchange crypto to fiat, fiat to crypto and crypto to crypto with simplicity and almost no fees.</p>
+                        </Col>
+                      </Row> :
+                      null
+                    }
+                  </Row>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <Row className={`${styles.questionBox} mb-4`}>
+                    <Col xs={10}>
+                      <p className={styles.question}>Can someone in any country send money to any other country via your exchange?</p>
+                    </Col>
+                    <Col
+                      xs={1}
+                      className={isOpen === '17' ? `${styles.close}` : `${styles.open}`}
+                      onClick={this.openQuestion}
+                      data-index='17'>
+                    </Col>
+                    { isOpen === '17' ?
+                      <Row>
+                        <Col lg={10}>
+                          <p className={styles.answer}>In the initial phase fiat currency transactions will be enabled in UK and other EU countries. XCOYNZ mission is to be a global exchange allowing transfers across all countries subject to local regulations. There are no geographical restrictions in exchanging crypto-currencies.</p>
+                        </Col>
+                      </Row> :
+                      null
+                    }
+                  </Row>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <Row className={`${styles.questionBox} mb-4`}>
+                    <Col xs={10}>
+                      <p className={styles.question}>How safe is the XCOYNZ Smart Algorithm Wallet?</p>
+                    </Col>
+                    <Col
+                      xs={1}
+                      className={isOpen === '18' ? `${styles.close}` : `${styles.open}`}
+                      onClick={this.openQuestion}
+                      data-index='18'>
+                    </Col>
+                    { isOpen === '18' ?
+                      <Row>
+                        <Col lg={10}>
+                          <p className={styles.answer}>Our Platform will have multiple layers of security, one of which will be two-factor authentication for Individual Smart Algorithm Wallets.</p>
+                        </Col>
+                      </Row> :
+                      null
+                    }
+                  </Row>
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <Row className={`${styles.questionBox} mb-4`}>
+                    <Col xs={10}>
+                      <p className={styles.question}>Why is the XCOYNZ Smart Algorithm Wallet better than other wallets?</p>
+                    </Col>
+                    <Col
+                      xs={1}
+                      className={isOpen === '19' ? `${styles.close}` : `${styles.open}`}
+                      onClick={this.openQuestion}
+                      data-index='19'>
+                    </Col>
+                    { isOpen === '19' ?
+                      <Row>
+                        <Col lg={10}>
+                          <p className={styles.answer}>The unique nature of the Smart Algorithm Wallet is such that it uses algorithms to provide strategic guidance where other wallets simply hold a single crypto asset. It combines multiple assets into a single entity but also has the ability to make recommendations on which of the components should be sold immediately or used to offset network fees and which should be held for long-term benefits (HODL). No other wallets provide intelligence in this way for maximising your profits and minimising network fees.</p>
+                        </Col>
+                      </Row> :
+                      null
+                    }
+                  </Row>
+                </Col>
+              </Row>
+              {/*<Row>
+                <Col>
+                  <Row className={`${styles.questionBox} mb-4`}>
+                    <Col xs={10}>
+                      <p className={styles.question}>Can I connect my PayPal and/or other banks to the XCOYNZ Exchange?</p>
+                    </Col>
+                    <Col
+                      xs={1}
+                      className={isOpen === '20' ? `${styles.close}` : `${styles.open}`}
+                      onClick={this.openQuestion}
+                      data-index='20'>
+                    </Col>
+                    { isOpen === '20' ?
+                      <Row>
+                        <Col lg={10}>
+                          <p className={styles.answer}>Can I connect my PayPal and/or other banks to the XCOYNZ Exchange?</p>
+                        </Col>
+                      </Row> :
+                      null
+                    }
+                  </Row>
+                </Col>
+              </Row>*/}
             </Col> : null
           }
         </Row>
